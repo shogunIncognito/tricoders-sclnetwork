1ab9f703752c11a64f07a2d31bcc86cd721bc593
shogunIncognito <saidsamuel2006@hotmail.com>
2024-04-09 12:32:07 -0500

    sidebar

--- src/app/dash/page.tsx
+++ src/app/dash/page.tsx
@@ -7,6 +7,7 @@ import { Card } from '@/components/ui/card'
 import useFetch from '@/hooks/useFetch'
 import { HeartIcon, MessageSquareIcon } from 'lucide-react'
 import { useFetchPostsResponse } from '../../../types'
+import Link from 'next/link'
 
 export default function Posts (): JSX.Element {
   const { data: posts, error, loading, setValue }: useFetchPostsResponse = useFetch('posts')
@@ -15,9 +16,10 @@ export default function Posts (): JSX.Element {
   if (error) return <p>Error: {error.message}</p>
 
   return (
-    <>
-      <AddPost setPost={setValue} />
-      <main className='overflow-auto gap-2 w-full flex flex-col items-center'>
+    <div className='flex flex-grow overflow-hidden'>
+
+      <main className='overflow-auto gap-2 w-full flex-1 h-full flex flex-col items-center'>
+        <AddPost setPost={setValue} />
         {posts?.map(post => (
           <div key={post._id} className='grid w-full max-w-full sm:max-w-[80%] md:max-w-[66%] lg:max-w-[40%] gap-4 p-4 place-content-center'>
             <Card className='p-4 space-y-4 w-full'>
@@ -72,6 +74,26 @@ export default function Posts (): JSX.Element {
           </div>
         ))}
       </main>
-    </>
+
+      {/* lista de usuarios que se estan siguiendo */}
+      <aside className='hidden lg:flex flex-col w-64 border-r border-gray-700 bg-gray-900/40'>
+        <div className='flex items-center gap-4 px-6 py-2'>
+          <span className='text-lg font-semibold text-gray-50'>Siguiendo</span>
+        </div>
+        <div className='flex-1 overflow-auto py-2'>
+          <nav className='grid items-start px-4 text-sm font-medium'>
+            <Link href='/'>
+              <img
+                alt='Profile image'
+                className='rounded-full'
+                height='40'
+                src='https://randomuser.me/api/portraits'
+              />
+              <span>Username</span>
+            </Link>
+          </nav>
+        </div>
+      </aside>
+    </div>
   )
 }
